bf78a8e8e0ed7ec43d51bc48c32245c9cf4c7eea
ljx213101212 <lijixiang.nus@gmail.com>
2022-10-15 21:05:51 +0800

    add Auto steps, locks, instant drop with basic gameover condition

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,89 +4,104 @@ using Util;
 
 public class Board : MonoBehaviour
 {
-    public Tilemap tilemap { get; private set; }
-    public Piece activePiece { get; private set; }
-    public TetrominoData[] tetrominoes;
-    public Vector2Int boardSize = new Vector2Int(10, 20);
-    public Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
+  public Tilemap tilemap { get; private set; }
+  public Piece activePiece { get; private set; }
+  public TetrominoData[] tetrominoes;
+  public Vector2Int boardSize = new Vector2Int(10, 20);
+  public Vector3Int spawnPosition = new Vector3Int(-1, 8, 0);
 
-    public RectInt Bounds
+  public RectInt Bounds
+  {
+    get
     {
-        get
-        {
-            Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
-            return new RectInt(position, boardSize);
-        }
+      Vector2Int position = new Vector2Int(-boardSize.x / 2, -boardSize.y / 2);
+      return new RectInt(position, boardSize);
+    }
+  }
+
+  private void Awake()
+  {
+    tilemap = GetComponentInChildren<Tilemap>();
+    activePiece = GetComponentInChildren<Piece>();
+
+    for (int i = 0; i < tetrominoes.Length; i++)
+    {
+      tetrominoes[i].Initialize();
     }
 
-    private void Awake()
+    Debug.Log(TetrisUtil.GetProjectName() + "Awake!");
+  }
+
+  private void Start()
+  {
+    SpawnPiece();
+  }
+
+  public void SpawnPiece()
+  {
+    int random = Random.Range(0, tetrominoes.Length);
+    TetrominoData data = tetrominoes[random];
+
+    activePiece.Initialize(this, spawnPosition, data);
+    Debug.Log("SpawnPiece:" + Time.time);
+
+    //TODO: Show Gameover Dialog
+    if (IsGameOver())
     {
-        tilemap = GetComponentInChildren<Tilemap>();
-        activePiece = GetComponentInChildren<Piece>();
+      tilemap.ClearAllTiles();
+    }
+    else
+    {
+      Set(activePiece);
+    }
+  }
 
-        for (int i = 0; i < tetrominoes.Length; i++)
-        {
-            tetrominoes[i].Initialize();
-        }
+  public void Set(Piece piece)
+  {
+    for (int i = 0; i < piece.cells.Length; i++)
+    {
+      Vector3Int tilePosition = piece.cells[i] + piece.position;
+      tilemap.SetTile(tilePosition, piece.data.tile);
+    }
+  }
 
-        Debug.Log(TetrisUtil.GetProjectName() + "Awake!");
+  public void Clear(Piece piece)
+  {
+    for (int i = 0; i < piece.cells.Length; i++)
+    {
+      Vector3Int tilePosition = piece.cells[i] + piece.position;
+      tilemap.SetTile(tilePosition, null);
+    }
+  }
+
+  //The position is only valid if every cell is valid
+  public bool IsPositionValid(Piece piece, Vector3Int newPosition)
+  {
+    for (int i = 0; i < piece.cells.Length; i++)
+    {
+      Vector3Int newTilePosition = piece.cells[i] + newPosition;
+
+      Debug.Log("checking isPositionValid: " + piece.cells[i]);
+      Debug.Log("checking isPositionValid new: " + newTilePosition);
+
+      //1. An out of bounds tile is invalid
+      if (!Bounds.Contains((Vector2Int)newTilePosition))
+      {
+        return false;
+      }
+
+      //2. A tile already occupies the position, thus invalid
+      if (tilemap.GetTile(newTilePosition))
+      {
+        return false;
+      }
     }
 
-    private void Start()
-    {
-        SpawnPiece();
-    }
+    return true;
+  }
 
-    public void SpawnPiece()
-    {
-        int random = Random.Range(0, tetrominoes.Length);
-        TetrominoData data = tetrominoes[random];
-
-        activePiece.Initialize(this, spawnPosition, data);
-        Set(activePiece);
-    }
-
-    public void Set(Piece piece)
-    {
-        for (int i = 0; i < piece.cells.Length; i++)
-        {
-            Vector3Int tilePosition = piece.cells[i] + piece.position;
-            tilemap.SetTile(tilePosition, piece.data.tile);
-        }
-    }
-
-    public void Clear(Piece piece)
-    {
-        for (int i = 0; i < piece.cells.Length; i++)
-        {
-            Vector3Int tilePosition = piece.cells[i] + piece.position;
-            tilemap.SetTile(tilePosition, null);
-        }
-    }
-
-    //The position is only valid if every cell is valid
-    public bool isPositionValid(Piece piece, Vector3Int newPosition)
-    {
-        for (int i = 0; i < piece.cells.Length; i++)
-        {
-            Vector3Int newTilePosition = piece.cells[i] + newPosition;
-
-            Debug.Log("checking isPositionValid: " + piece.cells[i]);
-            Debug.Log("checking isPositionValid new: " + newTilePosition);
-
-            //1. An out of bounds tile is invalid
-            if (!Bounds.Contains((Vector2Int)newTilePosition))
-            {
-                return false;
-            }
-
-            //2. A tile already occupies the position, thus invalid
-            if (tilemap.GetTile(newTilePosition))
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
+  public bool IsGameOver()
+  {
+    return !IsPositionValid(activePiece, spawnPosition);
+  }
 }
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -2,66 +2,113 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    public Board board { get; private set; }
-    public TetrominoData data { get; private set; }
-    public Vector3Int[] cells { get; private set; }
-    public Vector3Int position { get; private set; }
+  public Board board { get; private set; }
+  public TetrominoData data { get; private set; }
+  public Vector3Int[] cells { get; private set; }
+  public Vector3Int position { get; private set; }
 
-    public void Initialize(Board board, Vector3Int position, TetrominoData data)
+  public float stepDelay = 1f;
+  public float lockDelay = 0.05f;
+  private float stepTime;
+  private float lockTime;
+  public void Initialize(Board board, Vector3Int position, TetrominoData data)
+  {
+    this.board = board;
+    this.data = data;
+    this.position = position;
+
+    this.stepTime = Time.time + stepDelay;
+    this.lockTime = 0f;
+
+    if (cells == null)
     {
-        this.board = board;
-        this.data = data;
-        this.position = position;
-
-        if (cells == null)
-        {
-            cells = new Vector3Int[data.cells.Length];
-        }
-
-        for (int i = 0; i < cells.Length; i++)
-        {
-            cells[i] = (Vector3Int)data.cells[i];
-        }
+      cells = new Vector3Int[data.cells.Length];
     }
 
-    void Update()
+    for (int i = 0; i < cells.Length; i++)
     {
-        board.Clear(this);
+      cells[i] = (Vector3Int)data.cells[i];
+    }
+  }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Debug.Log("LeftArrow was pressed.");
-            Move(Vector2Int.left);
-        }
+  //https://docs.unity3d.com/2019.3/Documentation/Manual/TimeFrameManagement.html
+  void Update()
+  {
+    //one
+    lockTime += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Debug.Log("RightArrow was pressed.");
-            Move(Vector2Int.right);
-        }
+    board.Clear(this);
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Move(Vector2Int.down);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space)) { }
-
-        board.Set(this);
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+      Debug.Log("LeftArrow was pressed.");
+      Move(Vector2Int.left);
+    }
+    if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+      Debug.Log("RightArrow was pressed.");
+      Move(Vector2Int.right);
+    }
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      Move(Vector2Int.down);
+    }
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      InstantDrop();
     }
 
-    public void Move(Vector2Int translation)
+    if (Time.time > this.stepTime)
     {
-        Vector3Int newPosition = position;
-        newPosition.x += translation.x;
-        newPosition.y += translation.y;
-
-        bool isNextMoveValid = board.isPositionValid(this, newPosition);
-
-        Debug.Log("Moving: " + isNextMoveValid);
-        if (isNextMoveValid)
-        {
-            position = newPosition;
-        }
+      this.Step();
     }
+    board.Set(this);
+  }
+
+  public bool Move(Vector2Int translation)
+  {
+    Vector3Int newPosition = position;
+    newPosition.x += translation.x;
+    newPosition.y += translation.y;
+
+    bool isNextMoveValid = board.IsPositionValid(this, newPosition);
+
+    Debug.Log("Moving: " + isNextMoveValid);
+    if (isNextMoveValid)
+    {
+      position = newPosition;
+      lockTime = 0f; // reset lock time
+      return true;
+    }
+    return false;
+  }
+
+  private void Step()
+  {
+
+    this.stepTime = Time.time + this.stepDelay;
+
+    if (!Move(Vector2Int.down))
+    {
+      Debug.Log("Touch bottom Time:" + Time.time);
+      Lock();
+    }
+  }
+
+  private void Lock()
+  {
+    //Render Tile Map
+    board.Set(this);
+
+    //Init the next piece by using current Piece object
+    board.SpawnPiece();
+    //TODO clear lines
+  }
+
+  private void InstantDrop()
+  {
+    while (Move(Vector2Int.down)) { }
+
+    this.stepTime = 0f;
+  }
 }
