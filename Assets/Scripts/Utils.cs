aefb48007d96d513267f6eb1c5353142affde437
ljx213101212 <lijixiang.nus@gmail.com>
2022-10-18 22:16:24 +0800

    fix hardDroping glitch

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 namespace Util
 {
+  public enum Actions
+  {
+    MOVE, ROTATE
+  }
   public class TetrisUtil
   {
     public static string GetProjectName()
@@ -40,6 +44,11 @@ namespace Util
     public static bool IsOverlapping(List<Vector3Int> position1, List<Vector3Int> position2)
     {
       return position1.Any(p1 => position2.Any(p2 => p1.x == p2.x && p1.y == p2.y));
+    }
+
+    public static void DoActions(Actions actions)
+    {
+      
     }
   }
 }
